ed1fa97e1852d5952e1b4f830809bf262339740f
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-02-15 20:39:09 -0600

    Inventory Management System

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,32 @@
 // Write your code here
 
 
+let products=["Laptop","Phone","Headphones","Monitor"];
+function logFirstProduct(products) {
+  console.log(products[0]);
+}
+
+function addProduct(products, newitem) {
+  products.push(newitem);
+  return products;
+}
+addProduct(products, "Keyboard")
+console.log(products); 
+
+
+function updateProductName(products, position) {
+  products[position] = "Earbuds";
+}
+updateProductName(products, 2)
+console.log(products);
+
+function removeLastProduct(products, last) {
+  products.splice(0,last);
+}
+removeLastProduct(products, 4)
+console.log(products);
+
+
 
 // Export the necessary parts for testing
 module.exports = {
